e02f8af9527009bb7165cc6ed24f286df5ba5eb7
ZucchiniSky <scottbommarito@gmail.com>
2014-12-03 14:43:15 -0500

    snl fixes

--- p4/snl.cpp
+++ p4/snl.cpp
@@ -3,6 +3,7 @@
 #include "sort.h"
 #include "index.h"
 #include <map>
+#include <string.h>
 
 Status Operators::SNL(const string& result,           // Output relation name
                       const int projCnt,              // Number of attributes in the projection
@@ -39,7 +40,7 @@ Status Operators::SNL(const string& result,           // Output relation name
     RID secondRID;
     Record secondRecord;
 
-    s = rel1.startScan(0, 0, INTEGER, nullptr, NOTSET);
+    s = rel1.startScan(0, 0, INTEGER, NULL, NOTSET);
     if (s != OK) return s;
 
     while ((s = rel1.scanNext(firstRID, firstRecord)) != FILEEOF)
@@ -53,20 +54,20 @@ Status Operators::SNL(const string& result,           // Output relation name
         switch(attrDesc1.attrType)
         {
             case INTEGER:
-                filter = new char*(sizeof(int));
+                filter = new char[sizeof(int)];
                 memcpy(&filter, (char *) firstRecord.data + attrDesc1.attrOffset, sizeof(int));
                 break;
             case DOUBLE:
-                filter = new char*(sizeof(double));
+                filter = new char[sizeof(double)];
                 memcpy(&filter, (char *) firstRecord.data + attrDesc1.attrOffset, sizeof(double));
                 break;
             case STRING:
-                filter = new char*(sizeof(attrDesc1.attrLen));
+                filter = new char[attrDesc1.attrLen];
                 memcpy(&filter, (char *) firstRecord.data + attrDesc1.attrOffset, sizeof(attrDesc1.attrLen));
                 break;
         }
 
-        s = rel2.startScan(attrDesc2.attrOffset, attrDesc2.attrLen, attrDesc2.attrType, filter, op);
+        s = rel2.startScan(attrDesc2.attrOffset, attrDesc2.attrLen, static_cast<Datatype>(attrDesc2.attrType), filter, op);
         if (s != OK) return s;
 
         while ((s = rel2.scanNext(secondRID, secondRecord)) != FILEEOF)
@@ -74,12 +75,13 @@ Status Operators::SNL(const string& result,           // Output relation name
             if (s != OK) {
                 return s;
             }
-            void *data = new(size);
+
+            char *data = new char[size];
 
             for (int i = 0; i < projCnt; i++)
             {
-                AttrDesc currAttr = resultAttrDesc[attrMap[projNames[i].attrName]];
-                memcpy(data + currAttr.attrOffset, nextRecord.data, currAttr.attrLen);
+                AttrDesc currAttr = resultAttrDesc[attrMap[static_cast<char*>(projNames[i].attrName)]];
+                memcpy(data + currAttr.attrOffset, static_cast<char*>(nextRecord.data), currAttr.attrLen);
             }
 
             Record record;
@@ -92,24 +94,7 @@ Status Operators::SNL(const string& result,           // Output relation name
             s = resultFile.insertRecord(record, rid);
             if (s != OK) return s;
 
-            /*int comp = matchRec(firstRecord, secondRecord, attrDesc1, attrDesc2);
-
-            bool match = false;
-
-            if (comp == -1)
-            {
-                match = (op == LT || op == LTE || op == NE);
-            } else if (comp == 0)
-            {
-                match = (op == LTE || op == EQ || op == GTE);
-            } else
-            {
-                match = (op == GT || op == GTE || op == NE);
-            }
-
-            if (match)
-            {
-            }*/
+            delete data;
         }
 
         rel2.endScan();
